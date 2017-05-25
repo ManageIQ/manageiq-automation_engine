9bcbb8542eda1b3f51974f78e837e953c22f38e1
mkanoor <mkanoor@redhat.com>
2017-05-25 12:00:28 -0400

    Removed the if vmware check

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-vmware-infra_manager-host_esx.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-vmware-infra_manager-host_esx.rb
@@ -3,22 +3,22 @@ module MiqAeMethodService
     include MiqAeServiceEmsOperationsMixin
     require_relative "mixins/miq_ae_service_retirement_mixin"
     def shutdown(force = false)
-      sync_or_async_ems_operation(false, "vim_shutdown", [force]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_shutdown", [force])
       true
     end
 
     def reboot(force = false)
-      sync_or_async_ems_operation(false, "vim_reboot", [force]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_reboot", [force])
       true
     end
 
     def enter_maintenance_mode(timeout = 0, evacuate = false)
-      sync_or_async_ems_operation(false, "vim_enter_maintenance_mode", [timeout, evacuate]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_enter_maintenance_mode", [timeout, evacuate])
       true
     end
 
     def exit_maintenance_mode(timeout = 0)
-      sync_or_async_ems_operation(false, "vim_exit_maintenance_mode", [timeout]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_exit_maintenance_mode", [timeout])
       true
     end
 
@@ -27,27 +27,27 @@ module MiqAeMethodService
     end
 
     def power_down_to_standby(timeout = 0, evacuate = false)
-      sync_or_async_ems_operation(false, "vim_power_down_to_standby", [timeout, evacuate]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_power_down_to_standby", [timeout, evacuate])
       true
     end
 
     def power_up_from_standby(timeout = 0)
-      sync_or_async_ems_operation(false, "vim_power_up_from_standby", [timeout]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_power_up_from_standby", [timeout])
       true
     end
 
     def enable_vmotion(device = nil)
-      sync_or_async_ems_operation(false, "vim_enable_vmotion", [device]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_enable_vmotion", [device])
       true
     end
 
     def disable_vmotion(device = nil)
-      sync_or_async_ems_operation(false, "vim_disable_vmotion", [device]) if @object.is_vmware?
+      sync_or_async_ems_operation(false, "vim_disable_vmotion", [device])
       true
     end
 
     def vmotion_enabled?(device = nil)
-      object_send(:vim_vmotion_enabled?, device) if @object.is_vmware?
+      object_send(:vim_vmotion_enabled?, device)
     end
   end
 end
