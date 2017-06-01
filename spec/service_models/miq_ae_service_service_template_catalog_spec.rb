c3e3c37dfc409cd7c15fa9482fb97749c1892fb2
Fabien Dupont <fdupont@redhat.com>
2017-06-01 12:21:46 +0200

    Updated typos in spec and corrected miq-bot error

--- spec/service_models/miq_ae_service_service_template_catalog_spec.rb
+++ spec/service_models/miq_ae_service_service_template_catalog_spec.rb
@@ -3,7 +3,7 @@ module MiqAeServiceServiceTemplateCatalogSpec
     context "through an automation method" do
       before(:each) do
         Spec::Support::MiqAutomateHelper.create_service_model_method('SPEC_DOMAIN', 'EVM', 'AUTOMATE', 'test1', 'test')
-        @ae_method     = ::MiqAeMethod.first
+        @ae_method = ::MiqAeMethod.first
         @ae_result_key = 'foo'
         @service_template_catalog = FactoryGirl.create(:service_template_catalog)
         @user = FactoryGirl.create(:user_with_group)
@@ -16,18 +16,17 @@ module MiqAeServiceServiceTemplateCatalogSpec
 
     context "associations" do
       before do
-        service_template_catalog          = FactoryGirl.create(:service_template_catalog)
-        @service_service_template_catalog  = MiqAeMethodService::MiqAeServiceServiceTemplateCatalog.find(service_template_catalog.id)
+        service_template_catalog = FactoryGirl.create(:service_template_catalog)
+        @service_service_template_catalog = MiqAeMethodService::MiqAeServiceServiceTemplateCatalog.find(service_template_catalog.id)
       end
 
       it "#service_templates" do
         service_template = FactoryGirl.create(:service_template, :service_template_catalog_id => @service_service_template_catalog.id)
         first_service_template = @service_service_template_catalog.service_templates.first
 
-        expect(first_service_template).to    be_kind_of(MiqAeMethodService::MiqAeServiceService)
+        expect(first_service_template).to be_kind_of(MiqAeMethodService::MiqAeServiceService)
         expect(first_service_template.id).to eq(service_template.id)
       end
-
     end
   end
 end
