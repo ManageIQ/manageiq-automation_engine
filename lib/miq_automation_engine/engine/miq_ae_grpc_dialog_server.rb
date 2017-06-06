88789d0d97f085676930ae1b933b23a4ce7b1b59
mkanoor <mkanoor@redhat.com>
2017-06-06 18:38:19 -0400

    Fixed rubocop warnings

--- lib/miq_automation_engine/engine/miq_ae_grpc_dialog_server.rb
+++ lib/miq_automation_engine/engine/miq_ae_grpc_dialog_server.rb
@@ -14,7 +14,7 @@ class MiqAeGrpcDialogServer < Manageiq::Dialog::AutomateDialog::Service
 
   def get_dialog_field_date_time_control(input, _unused_call)
     attributes = call_automate(input).root.attributes
-    result =  Manageiq::Dialog::DialogFieldDateTimeControlResult.new
+    result = Manageiq::Dialog::DialogFieldDateTimeControlResult.new
     result.show_past_dates = attributes[:show_past_dates] || false
     result.read_only       = attributes[:read_only]       || false
     result.visible         = attributes[:visible]         || true
@@ -24,14 +24,14 @@ class MiqAeGrpcDialogServer < Manageiq::Dialog::AutomateDialog::Service
 
   def get_dialog_field_sorted_item(input, _unused_call)
     attributes = call_automate(input).root.attributes
-    valuesMap = Google::Protobuf::Map.new(:int64, :string)
-    attributes['values'].each { |k, v| valuesMap[k] = v }
+    values_map = Google::Protobuf::Map.new(:int64, :string)
+    attributes['values'].each { |k, v| values_map[k] = v }
     result = Manageiq::Dialog::DialogFieldSortedItemResult.new
     result.sort_by       = attributes[:sort_by] || 'description'
     result.data_type     = attributes[:data_type] || 'string'
     result.required      = attributes[:required]  || false
     result.sort_order    = attributes[:sort_order] || 'ascending'
-    result.values        = valuesMap
+    result.values        = values_map
     result.default_value = ""
     result
   end
@@ -39,7 +39,7 @@ class MiqAeGrpcDialogServer < Manageiq::Dialog::AutomateDialog::Service
   def get_dialog_field_text_area_box(input, _unused_call)
     puts "Method called #{input.to_h}"
     attributes = call_automate(input).root.attributes
-    result =  Manageiq::Dialog::DialogFieldTextAreaBoxResult.new
+    result = Manageiq::Dialog::DialogFieldTextAreaBoxResult.new
     result.required        = attributes[:required]  || false
     result.read_only       = attributes[:read_only] || false
     result.visible         = attributes[:visible]   || true
@@ -49,7 +49,7 @@ class MiqAeGrpcDialogServer < Manageiq::Dialog::AutomateDialog::Service
 
   def get_dialog_field_text_box(input, _unused_call)
     attributes = call_automate(input).root.attributes
-    result =  Manageiq::Dialog::DialogFieldTextBoxResult.new
+    result = Manageiq::Dialog::DialogFieldTextBoxResult.new
     result.data_type       = attributes[:data_type] || 'string'
     result.protected       = attributes[:protected] || false
     result.required        = attributes[:required]  || false
@@ -63,7 +63,7 @@ class MiqAeGrpcDialogServer < Manageiq::Dialog::AutomateDialog::Service
 
   def get_dialog_field_check_box(input, _unused_call)
     attributes = call_automate(input).root.attributes
-    result =  Manageiq::Dialog::DialogFieldCheckBoxResult.new
+    result = Manageiq::Dialog::DialogFieldCheckBoxResult.new
     result.required        = attributes[:required]  || true
     result.read_only       = attributes[:read_only] || false
     result.visible         = attributes[:visible]   || true
