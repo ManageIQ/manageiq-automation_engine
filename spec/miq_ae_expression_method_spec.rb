fc2194032db34954400fe63eb3c869eafb0cf49d
mkanoor <mkanoor@redhat.com>
2017-07-11 16:26:27 -0400

    Changed let to let!

--- spec/miq_ae_expression_method_spec.rb
+++ spec/miq_ae_expression_method_spec.rb
@@ -3,9 +3,9 @@ module MiqAeExpressionMethodSpec
   describe MiqAeExpressionMethod do
     include Spec::Support::AutomationHelper
     let(:user) { FactoryGirl.create(:user_with_group) }
-    let(:vm1) { FactoryGirl.create(:vm, :name => 'test_2.1', :cpu_shares => 400) }
-    let(:vm2) { FactoryGirl.create(:vm, :name => 'test_3.1', :cpu_shares => 400) }
-    let(:vm3) { FactoryGirl.create(:vm, :name => 'test_4.2', :cpu_shares => 11) }
+    let!(:vm1) { FactoryGirl.create(:vm, :name => 'test_2.1', :cpu_shares => 400) }
+    let!(:vm2) { FactoryGirl.create(:vm, :name => 'test_3.1', :cpu_shares => 400) }
+    let!(:vm3) { FactoryGirl.create(:vm, :name => 'test_4.2', :cpu_shares => 11) }
     let(:complex_qs_exp) do
       {"and" => [{"STARTS WITH" => {"field" => "Vm-name", "value" => :user_input}},
                  {"ENDS WITH"   => {"field" => "Vm-name", "value" => :user_input}}]}
@@ -27,9 +27,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method" do
-      vm1
-      vm2
-      vm3
       m_params['result_type'] = {'datatype' => 'string', 'default_value' => 'array'}
       create_ae_model_with_method(:ae_namespace => 'GAULS',
                                   :ae_class => 'ASTERIX', :instance_name => 'DOGMATIX',
@@ -42,9 +39,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method dialog_hash" do
-      vm1
-      vm2
-      vm3
       create_ae_model_with_method(:ae_namespace => 'GAULS',
                                   :ae_class => 'ASTERIX', :instance_name => 'DOGMATIX',
                                   :method_name => 'OBELIX', :method_params => m_params,
@@ -57,9 +51,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method no result" do
-      vm1
-      vm2
-      vm3
       m_params['arg1'] = {'datatype' => 'string',  'default_value' => 'nada'}
       create_ae_model_with_method(:ae_namespace => 'GAULS',
                                   :ae_class => 'ASTERIX', :instance_name => 'DOGMATIX',
@@ -72,9 +63,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method no result use default value" do
-      vm1
-      vm2
-      vm3
       m_params['arg1'] = {'datatype' => 'string', 'default_value' => 'nada'}
       m_params['default'] = {'datatype' => 'array', 'default_value' => 'nada'}
       m_params['on_empty'] = {'datatype' => 'string', 'default_value' => 'warn'}
@@ -90,9 +78,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method result attr" do
-      vm1
-      vm2
-      vm3
       m_params['result_attr'] = {'datatype' => 'string', 'default_value' => 'vitalstatistix'}
       m_params['result_type'] = {'datatype' => 'string', 'default_value' => 'array'}
       create_ae_model_with_method(:ae_namespace => 'GAULS',
@@ -106,9 +91,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method distinct" do
-      vm1
-      vm2
-      vm3
       m_params['distinct'] = {'datatype' => 'array', 'default_value' => 'cpu_shares'}
       m_params['result_type'] = {'datatype' => 'string', 'default_value' => 'array'}
       create_ae_model_with_method(:ae_namespace => 'GAULS',
@@ -122,8 +104,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method undefined function" do
-      vm1
-      vm2
       m_params['attributes'] = {'datatype' => 'array', 'default_value' => 'nada'}
       create_ae_model_with_method(:ae_namespace => 'GAULS',
                                   :ae_class => 'ASTERIX', :instance_name => 'DOGMATIX',
@@ -137,9 +117,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method target object missing" do
-      vm1
-      vm2
-      vm3
       m_params['result_obj'] = {'datatype' => 'string',  'default_value' => 'nada'}
       create_ae_model_with_method(:ae_namespace => 'GAULS',
                                   :ae_class => 'ASTERIX', :instance_name => 'DOGMATIX',
@@ -153,9 +130,6 @@ module MiqAeExpressionMethodSpec
     end
 
     it "expression_method invalid result type" do
-      vm1
-      vm2
-      vm3
       m_params['result_type'] = {'datatype' => 'string', 'default_value' => 'nada'}
 
       create_ae_model_with_method(:ae_namespace => 'GAULS',
