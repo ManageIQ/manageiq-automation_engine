ef49907af749547342c618763157b23f1abc9a6f
mkanoor <mkanoor@redhat.com>
2017-08-15 17:10:29 -0400

    Added ability to serialize and desrialize workspace

---
+++ lib/miq_automation_engine/engine/miq_ae_engine/miq_ae_reference.rb
@@ -0,0 +1,28 @@
+module MiqAeEngine
+  module MiqAeReference
+    def self.encode(value)
+      if value.kind_of?(Array)
+        value.map { |v| encode(v) }
+      elsif value.kind_of?(Hash)
+        value.each_with_object({}) { |(k, v), hash| hash[k] = encode(v) }
+      elsif /MiqAeMethodService::/ =~ value.class.to_s
+        "vmdb_reference::#{value.href_slug}"
+      else
+        value
+      end
+    end
+
+    def self.decode(value, user)
+      if value.kind_of?(Array)
+        value.map { |v| decode(v, user) }
+      elsif value.kind_of?(Hash)
+        value.each_with_object({}) { |(k, v), hash| hash[k] = decode(v, user) }
+      elsif value.kind_of?(String) && /vmdb_reference::(.*)/.match(value)
+        obj = Api::Utils.resource_search_by_href_slug($1, user)
+        MiqAeMethodService::MiqAeServiceModelBase.wrap_results(obj)
+      else
+        value
+      end
+    end
+  end
+end
