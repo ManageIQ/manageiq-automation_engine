301afaae08a0528d24c47ac9abdf9fc1a09035eb
Boris Odnopozov <bodnopoz@redhat.com>
2017-08-21 15:43:57 +0300

    The vm removal was based on refresh failing with exception. It is now based on an explicit check to validate if it exists on the provider or not.
    This is part of a fix for: https://bugzilla.redhat.com/show_bug.cgi?id=1478108

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
@@ -3,5 +3,6 @@ module MiqAeMethodService
     expose :validate_import_vm
     expose :submit_import_vm
     expose :submit_configure_imported_vm_networks
+    expose :exists_on_provider?
   end
 end
