6c9ef4158c47bfeef0124a2e426e893c62732776
mkanoor <mkanoor@redhat.com>
2018-01-19 18:01:23 -0500

    PR feedback

--- lib/miq_automation_engine/engine/miq_ae_engine/miq_ae_builtin_method/miq_ae_task_builtin.rb
+++ lib/miq_automation_engine/engine/miq_ae_engine/miq_ae_builtin_method/miq_ae_task_builtin.rb
@@ -10,7 +10,7 @@ module MiqAeEngine
           raise ArgumentError, "miq_provision not specified" unless task
 
           result = task.statemachine_task_status
-          $miq_ae_logger.info("Builtin ProvisionCheck returned <#{result}> for state <#{task.state}> and status <#{task['status']}>")
+          $miq_ae_logger.info("Builtin check_provisioned returned <#{result}> for state <#{task.state}> and status <#{task['status']}>")
 
           set_ae_result(result, task, root, inputs)
         end
@@ -20,7 +20,7 @@ module MiqAeEngine
           when 'error'
             root['ae_result'] = 'error'
             reason = task.message
-            $miq_ae_logger.error("ProvisionCheck error <#{reason}>")
+            $miq_ae_logger.error("Builtin check_provisioned error <#{reason}>")
             reason = reason[7..-1] if reason[0..6] == 'Error: '
             root['ae_reason'] = reason
           when 'retry'
