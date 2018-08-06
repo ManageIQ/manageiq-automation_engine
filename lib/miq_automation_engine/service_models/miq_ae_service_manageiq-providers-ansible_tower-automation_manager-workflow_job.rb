256165eba4c3e065d8d64a7a513bd73ebd4f2fcc
Bill Wei <bilwei@redhat.com>
2018-08-06 12:01:01 -0400

    expose configuration_workflow and workflow_job

---
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-ansible_tower-automation_manager-workflow_job.rb
@@ -0,0 +1,12 @@
+module MiqAeMethodService
+  class MiqAeServiceManageIQ_Providers_AnsibleTower_AutomationManager_WorkflowJob < MiqAeServiceManageIQ_Providers_ExternalAutomationManager_OrchestrationStack
+    expose :workflow_template, :association => true
+    expose :refresh_ems
+
+    def self.create_job(template, args = {})
+      template_object = ConfigurationScript.find_by(:id => template.id)
+      klass = ManageIQ::Providers::AnsibleTower::AutomationManager::WorkflowJob
+      wrap_results(klass.create_job(template_object, args))
+    end
+  end
+end
