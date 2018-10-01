febc71ffec2d1f7f67b1765f8fa77bba9105f324
Madhu Kanoor <mkanoor@redhat.com>
2018-10-01 16:17:56 -0400

    Merge pull request #237 from lfu/encrypted_password_in_playbook_1602883
    Add some helper methods for MiqAeObject.
    
    (cherry picked from commit 28fe61df70bdd738d781cf2409e4a07af2dc8391)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1602883

---
+++ spec/engine/miq_ae_method_service/miq_ae_service_object_common_spec.rb
@@ -0,0 +1,44 @@
+describe MiqAeMethodService::MiqAeServiceObjectCommon do
+  let(:wrapper_class) do
+    Class.new do
+      include MiqAeMethodService::MiqAeServiceObjectCommon
+      def initialize(hash = {})
+        @object = Spec::Support::MiqAeMockObject.new(hash)
+      end
+    end
+  end
+
+  let(:test_hash) { {} }
+  let(:wrapper) { wrapper_class.new(test_hash) }
+
+  describe "#attributes" do
+    context "password fields" do
+      let(:test_hash) do
+        { 'fred'   => MiqAePassword.new("wilma") }
+      end
+
+      it "#attributes" do
+        enc_hash = {'fred' => MiqPassword::MASK, 'barney' => 'betty' }
+
+        wrapper['barney'] = 'betty'
+        expect(wrapper.attributes).to eq(enc_hash)
+      end
+
+      it "#decrypt" do
+        expect(wrapper.decrypt('fred')).to eq('wilma')
+      end
+
+      it "#[]" do
+        expect(wrapper['fred']).to eq(MiqPassword::MASK)
+      end
+
+      it "#encrypted?" do
+        expect(wrapper.encrypted?('fred')).to be true
+      end
+
+      it "#encrypted_string" do
+        expect(wrapper.encrypted_string('fred')).to eq(test_hash['fred'].encStr)
+      end
+    end
+  end
+end
