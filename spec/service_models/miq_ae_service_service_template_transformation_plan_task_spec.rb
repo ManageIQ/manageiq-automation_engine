e61cd5a9d5f7b451b67e9585145fd60549f7b85a
Fabien Dupont <fdupont@redhat.com>
2018-10-17 04:05:29 -0400

    Avoid intantiating engine in tests

--- spec/service_models/miq_ae_service_service_template_transformation_plan_task_spec.rb
+++ spec/service_models/miq_ae_service_service_template_transformation_plan_task_spec.rb
@@ -1,34 +1,20 @@
 describe MiqAeMethodService::MiqAeServiceServiceTemplateTransformationPlanTask do
-  let(:user) { FactoryGirl.create(:user_with_group) }
-  let(:task) { FactoryGirl.create(:service_template_transformation_plan_task) }
-
-  before(:each) do
-    Spec::Support::MiqAutomateHelper.create_service_model_method('SPEC_DOMAIN', 'EVM', 'AUTOMATE', 'test1', 'test')
-    @ae_method = ::MiqAeMethod.first
-  end
-
-  def invoke_ae
-    MiqAeEngine.instantiate("/EVM/AUTOMATE/test1?ServiceTemplateTransformationPlanTask::service_template_transformation_plan_task=#{task.id}", user)
-  end
+  let(:conversion_host_1) { FactoryGirl.create(:conversion_host) }
+  let(:service_conversion_host_1) { MiqAeMethodService::MiqAeServiceConversionHost.find(conversion_host_1.id) }
+  let(:conversion_host_2) { FactoryGirl.create(:conversion_host) }
+  let(:service_conversion_host_2) { MiqAeMethodService::MiqAeServiceConversionHost.find(conversion_host_2.id) }
+  let(:task) { FactoryGirl.create(:service_template_transformation_plan_task, :conversion_host => conversion_host_1) }
+  let(:service_task) { MiqAeMethodService::MiqAeServiceServiceTemplateTransformationPlanTask.find(task.id) }
 
   describe "#conversion_host=" do
-    before(:each) do
-      @conversion_host = FactoryGirl.create(:conversion_host)
-    end
-
     it "removes the conversion host if arg is nil" do
-      task.conversion_host = @conversion_host
-      method = "$evm.root['#{@ae_result_key}'] = $evm.root['service_template_transformation_plan_task'].conversion_host = nil"
-      @ae_method.update_attributes(:data => method)
-      invoke_ae
+      service_task.conversion_host = nil
       expect(task.reload.conversion_host).to be_nil
     end
 
     it "sets the conversion host if arg is not nil" do
-      method = "$evm.root['#{@ae_result_key}'] = $evm.root['service_template_transformation_plan_task'].conversion_host = $evm.vmdb('conversion_host').first"
-      @ae_method.update_attributes(:data => method)
-      invoke_ae
-      expect(task.reload.conversion_host).to eq(@conversion_host)
+      service_task.conversion_host = service_conversion_host_2
+      expect(task.reload.conversion_host).to eq(conversion_host_2)
     end
   end
 end
