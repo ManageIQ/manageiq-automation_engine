f828fe218a846e5b72e0d22885ee8e73c7d329dd
Lucy Fu <lufu@redhat.com>
2018-12-19 10:38:06 -0500

    Add helper method #create_job for Ansible Tower Job/Workflow template.
    https://bugzilla.redhat.com/show_bug.cgi?id=1658172

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-ansible_tower-automation_manager-configuration_workflow.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-ansible_tower-automation_manager-configuration_workflow.rb
@@ -1,5 +1,9 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_AnsibleTower_AutomationManager_ConfigurationWorkflow < MiqAeServiceManageIQ_Providers_ExternalAutomationManager_ConfigurationScript
     expose :run
+
+    def create_job(args)
+      ar_method { wrap_results(ManageIQ::Providers::AnsibleTower::AutomationManager::WorkflowJob.create_job(@object, args)) }
+    end
   end
 end
