d67f44cf0f666e636e1660f564428a9b0562c1ca
Miha Pleško <miha.plesko@xlab.si>
2019-03-19 15:43:56 +0100

    Physical server provisioning via StateMachine
    With this commit we make sure that AE engine calculates correct
    entrypoint when PhysicalServer provisioning request is created,
    namely:
    
    ```
    PhysicalInfrastructure/PhysicalServer/Lifecycle/Provisioning
    ```
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

---
+++ spec/service_models/miq_ae_service_physical_server_spec.rb
@@ -0,0 +1,30 @@
+describe MiqAeMethodService::MiqAeServicePhysicalServer do
+  it '#turn_on_loc_led' do
+    expect(described_class.instance_methods).to include(:turn_on_loc_led)
+  end
+
+  it '#turn_off_loc_led' do
+    expect(described_class.instance_methods).to include(:turn_off_loc_led)
+  end
+
+  it '#power_on' do
+    expect(described_class.instance_methods).to include(:power_on)
+  end
+
+  it '#power_off' do
+    expect(described_class.instance_methods).to include(:power_off)
+  end
+
+  describe '#emstype' do
+    before { allow(ems.class).to receive(:ems_type).and_return('THE_EMS_TYPE') }
+
+    let(:server) { FactoryBot.create(:physical_server, :ext_management_system => ems) }
+    let(:ems)    { FactoryBot.create(:ems_physical_infra) }
+
+    subject { MiqAeMethodService::MiqAeServicePhysicalServer.find(server.id) }
+
+    it 'passes on to ems' do
+      expect(subject.emstype).to eq('THE_EMS_TYPE')
+    end
+  end
+end
