5091d5bf4b233006a2106e4d787b346d06baf1ea
Satoe Imaishi <simaishi@redhat.com>
2019-05-07 15:50:46 -0400

    Revert "Merge pull request #309 from d-m-u/exposing_things_on_inframanagers"
    This reverts commit a046475081d6acd23d524afc73b120bbb2e5c2e9, reversing
    changes made to e94b528ab2f9257c78ddabfda43947887ac85ff8.

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-infra_manager.rb
+++
@@ -1,5 +0,0 @@
-module MiqAeMethodService
-  class MiqAeServiceManageIQ_Providers_InfraManager < MiqAeServiceManageIQ_Providers_BaseManager
-    expose :switches, :association => true
-  end
-end
--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
@@ -1,5 +1,5 @@
 module MiqAeMethodService
-  class MiqAeServiceManageIQ_Providers_Redhat_InfraManager < MiqAeServiceManageIQ_Providers_InfraManager
+  class MiqAeServiceManageIQ_Providers_Redhat_InfraManager < MiqAeServiceEmsInfra
     expose :supports_vm_import?
     expose :submit_import_vm
     expose :submit_configure_imported_vm_networks
