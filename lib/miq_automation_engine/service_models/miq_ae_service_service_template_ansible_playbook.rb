2874b662c2ebeb36fda9673e513e49597569de2d
Nick LaMuro <nicklamuro@gmail.com>
2019-11-15 19:45:09 -0600

    Fix manageiq-content specs on hammer
    The following PR was recently backported to `hammer`:
    
    https://github.com/ManageIQ/manageiq-content/pull/559
    
    This made use of the `config_info` association to accomplish its goals.
    It was possible to use this method without the changes made here since a
    PR was merged and available as of `ivanchuk` that exposed all service
    associations to Miq AE service models:
    
    https://github.com/ManageIQ/manageiq-automation_engine/pull/308
    
    But that PR was not merged to `hammer`.  Do to the risk of backporting
    that PR, this is a one off addition to allow the functionality of
    ManageIQ/manageiq-content#559 (low risk) to function without requiring a
    backport of everything in ManageIQ/manageiq-automation_engine#308 (high
    risk).

--- lib/miq_automation_engine/service_models/miq_ae_service_service_template_ansible_playbook.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_service_template_ansible_playbook.rb
@@ -1,5 +1,6 @@
 module MiqAeMethodService
   class MiqAeServiceServiceTemplateAnsiblePlaybook < MiqAeServiceServiceTemplateGeneric
+    expose :config_info, :association => true
     expose :job_template
   end
 end
