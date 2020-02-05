a52b40e0ecb7711152df56906617fdcbd90cc399
Adam Grare <agrare@redhat.com>
2020-02-05 13:06:37 -0500

    Fix CloudVolume spec

--- spec/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-cloud_volume_spec.rb
+++ spec/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-cloud_volume_spec.rb
@@ -1,18 +1,18 @@
 describe MiqAeMethodService::MiqAeServiceUser do
-  let(:cloud_volume)         { FactoryBot.create(:cloud_volume_openstack) }
+  let(:cloud_volume)         do
+    FactoryBot.create(:cloud_volume_openstack, :ext_management_system => FactoryBot.create(:ems_openstack))
+  end
   let(:service_cloud_volume) do
     MiqAeMethodService::MiqAeServiceManageIQ_Providers_Openstack_CloudManager_CloudVolume.find(cloud_volume.id)
   end
 
   before do
-    zone = FactoryBot.create(:zone)
-    allow_any_instance_of(CloudVolume).to receive(:my_zone).and_return(zone.name)
-    allow(MiqServer).to receive(:my_zone).and_return(zone.name)
     @base_queue_options = {
       :class_name  => cloud_volume.class.name,
       :instance_id => cloud_volume.id,
-      :zone        => zone.name,
+      :zone        => cloud_volume.ext_management_system.my_zone,
       :role        => 'ems_operations',
+      :queue_name  => cloud_volume.queue_name_for_ems_operations,
       :task_id     => nil
     }
   end
@@ -20,7 +20,7 @@ describe MiqAeMethodService::MiqAeServiceUser do
   it "#backup_create async" do
     service_cloud_volume.backup_create('test backup', false)
 
-    expect(MiqQueue.first).to have_attributes(
+    expect(MiqQueue.last).to have_attributes(
       @base_queue_options.merge(
         :method_name => 'backup_create',
         :args        => [{:name => "test backup", :incremental => false}])
@@ -30,7 +30,7 @@ describe MiqAeMethodService::MiqAeServiceUser do
   it "#backup_restore async" do
     service_cloud_volume.backup_restore('1234')
 
-    expect(MiqQueue.first).to have_attributes(
+    expect(MiqQueue.last).to have_attributes(
       @base_queue_options.merge(
         :method_name => 'backup_restore',
         :args        => ["1234"])
