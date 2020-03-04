fb2fb61f4a9b1cdf6d3afc65abe9ac0276a0e272
tina <tfitzger@redhat.com>
2020-03-04 14:48:15 -0500

    Merge pull request #429 from d-m-u/removing_to_xml
    Remove the to_xml call from miq_ae_engine
    
    (cherry picked from commit 1a3ba3a3cde0706890050e405f56b4c808213c1e)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1805451

--- lib/miq_automation_engine/engine/miq_ae_engine.rb
+++ lib/miq_automation_engine/engine/miq_ae_engine.rb
@@ -295,9 +295,7 @@ module MiqAeEngine
     raise "User object not passed in" unless user_obj.kind_of?(User)
     uri = create_automation_object(uri, attr, options) if attr
     options[:uri] = uri
-    MiqAeWorkspaceRuntime.instantiate(uri, user_obj, :readonly => readonly).tap do |ws|
-      $miq_ae_logger.debug { ws.to_expanded_xml }
-    end
+    MiqAeWorkspaceRuntime.instantiate(uri, user_obj, :readonly => readonly)
   end
 
   def self.ae_user_object(options = {})
