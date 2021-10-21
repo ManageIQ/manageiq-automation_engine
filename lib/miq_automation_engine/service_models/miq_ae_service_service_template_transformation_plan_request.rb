810af966c5688d8e51e97ecdf310a788d1040a78
Joe Rafaniello <jrafanie@gmail.com>
2021-10-21 13:09:04 -0400

    Remove v2v service models
    Part of https://github.com/ManageIQ/manageiq/issues/21379

--- lib/miq_automation_engine/service_models/miq_ae_service_service_template_transformation_plan_request.rb
+++
@@ -1,8 +0,0 @@
-module MiqAeMethodService
-  class MiqAeServiceServiceTemplateTransformationPlanRequest < MiqAeServiceServiceTemplateProvisionRequest
-    expose :source_vms, :association => true
-    expose :validate_conversion_hosts
-    expose :validate_vm, :association => true
-    expose :approve_vm, :association => true
-  end
-end
