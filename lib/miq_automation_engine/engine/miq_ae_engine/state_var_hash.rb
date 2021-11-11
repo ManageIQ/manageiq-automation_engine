015ed795f6c6d44ed28c54b6baefd6ccdd42c37d
Jason Frey <fryguy9@gmail.com>
2021-11-11 15:22:11 -0500

    Remove direct reference to VMDBLogger

--- lib/miq_automation_engine/engine/miq_ae_engine/state_var_hash.rb
+++ lib/miq_automation_engine/engine/miq_ae_engine/state_var_hash.rb
@@ -22,7 +22,7 @@ module MiqAeEngine
         binary_blob.destroy
 
         $miq_ae_logger.info("Reloading state var data: ")
-        VMDBLogger.log_hashes($miq_ae_logger, blob_hash, :filter => Vmdb::Settings.secret_filter)
+        $miq_ae_logger.log_hashes(blob_hash, :filter => Vmdb::Settings.secret_filter)
         update(blob_hash)
       else
         $miq_ae_logger.warn("Failed to load BinaryBlob with ID [#{coder[SERIALIZE_KEY]}] for #{self.class.name}")
