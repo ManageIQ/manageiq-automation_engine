3d5bcfcd2ed24a2d53cb18ac96743f9054856947
eilam <eilam.elimelech@kyndryl.com>
2022-08-23 13:07:12 +0300

    create_volume_task for creating volume with the automation engine

--- lib/miq_automation_engine/service_models/miq_ae_service_cloud_volume.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_cloud_volume.rb
@@ -7,4 +7,9 @@ module MiqAeMethodService
     expose :attach_volume,          :override_return => nil
     expose :detach_volume,          :override_return => nil
   end
+
+  def self.create_volume_task(ems, options = {})
+    ext_management_system = ExtManagementSystem.find(ems)
+    CloudVolume.create_volume_queue('admin',ext_management_system,options)
+  end
 end
