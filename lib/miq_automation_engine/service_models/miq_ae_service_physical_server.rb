4fb9f604881b749b3d25e78fe34f9e875b3be933
eilam <eilam.elimelech@kyndryl.com>
2022-11-20 17:18:33 +0200

    add deploy server profile from template

--- lib/miq_automation_engine/service_models/miq_ae_service_physical_server.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_physical_server.rb
@@ -4,5 +4,10 @@ module MiqAeMethodService
     expose :turn_off_loc_led
     expose :power_on
     expose :power_off
+
+    def self.create_server_profile_and_deploy_task(ems)
+      ext_management_system = ExtManagementSystem.find(ems)
+      PhysicalServerProfileTemplate.check
+    end
   end
 end
