12adaeaa1d8b89a805be9e956a84e9da131e2316
eilam <eilam.elimelech@kyndryl.com>
2022-11-22 23:16:10 +0200

    fix

--- lib/miq_automation_engine/service_models/miq_ae_service_physical_server.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_physical_server.rb
@@ -5,8 +5,11 @@ module MiqAeMethodService
     expose :power_on
     expose :power_off
 
-    def self.create_server_profile_and_deploy_task(template_id, server_id, profile_name)
-      PhysicalServerProfileTemplate.deploy_server_from_template(template_id, server_id, profile_name)
+    def self.create_server_profile_and_deploy_task(ems_id, template_id, server_id, profile_name)
+      ext_management_system = ExtManagementSystem.find(ems_id)
+      profile_template = PhysicalServerProfileTemplate.find(template_id)
+      task_id = profile_template.deploy_server_from_template_queue(ext_management_system, server_id, profile_name)
+      MiqTask.wait_for_taskid(task_id)
     end
   end
 end
