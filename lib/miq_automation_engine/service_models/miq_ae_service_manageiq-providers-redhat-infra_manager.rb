934cc2be9e06d524cb5c16997f87d8051b842510
Keenan Brock <keenan@thebrocks.net>
2024-02-29 14:35:19 -0500

    v2v drop supports_vm_import

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
@@ -1,6 +1,5 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Redhat_InfraManager < MiqAeServiceManageIQ_Providers_Ovirt_InfraManager
-    expose :supports_vm_import?
     expose :submit_import_vm
     expose :submit_configure_imported_vm_networks
   end
