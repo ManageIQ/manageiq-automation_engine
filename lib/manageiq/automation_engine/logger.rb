82b80a74ae2451951966954071716e3db1c07c27
Adam Grare <adam@grare.com>
2025-04-24 11:56:22 -0400

    Use .wrap for RequestLogger

--- lib/manageiq/automation_engine/logger.rb
+++ lib/manageiq/automation_engine/logger.rb
@@ -1,29 +1,25 @@
 module ManageIQ
   module AutomationEngine
     class Logger < ManageIQ::Loggers::Base
-      attr_reader :automation_log_wrapper, :resource_id
+      attr_reader :resource_id
 
-      def initialize(*args, automation_log_wrapper:, resource_id: nil, **kwargs)
-        @automation_log_wrapper = automation_log_wrapper
-        @resource_id            = resource_id
+      def initialize(*args, resource_id: nil, **kwargs)
+        @resource_id = resource_id
 
         super(*args, **kwargs)
       end
 
-      def self.create_log_wrapper(io: File::NULL, automation_log_wrapper: nil, resource_id: nil)
-        # We modify the interface of logger methods such as info/warn/etc. to allow the keyword argument
-        # resource_id. Therefore, we need to wrap all client logger calls to these methods to process the resource_id,
-        # cut the request_log entry and forward the remaining arguments to the logger.
-        new(io, :progname => "automation", :automation_log_wrapper => automation_log_wrapper || Vmdb::Loggers.create_logger("automation.log"), :resource_id => resource_id)
+      def self.create_log_wrapper(log_wrapper: nil, resource_id: nil)
+        log_wrapper ||= $miq_ae_logger
+
+        new(nil, :progname => "automation", :resource_id => resource_id).wrap(log_wrapper)
       end
 
       private
 
       def add(severity, message = nil, progname = nil, &block)
-        automation_log_wrapper.add(severity, message, progname, &block)
-
         severity ||= Logger::UNKNOWN
-        return true if severity < @level
+        return true if severity < @level || resource_id.nil?
 
         progname ||= @progname
 
@@ -36,9 +32,9 @@ module ManageIQ
           end
         end
 
-        RequestLog.create(:message => message, :severity => format_severity(severity), :resource_id => resource_id) if resource_id
+        RequestLog.create(:message => message, :severity => format_severity(severity), :resource_id => resource_id)
 
-        super
+        true
       end
     end
   end
